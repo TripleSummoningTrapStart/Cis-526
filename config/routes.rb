2777cc963bf7be643c0b1a8af0710f2a888eb123
Josh Reed <Joshua25@ksu.edu>
2015-03-03 15:31:00 -0600

    Moved the folder

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Labyrinth::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  get 'gps#gps'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
