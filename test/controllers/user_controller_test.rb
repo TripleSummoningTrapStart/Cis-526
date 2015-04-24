c3167a6f212acd70715fd47888e83d5083d4c06d
TheRealPino <Seanpino@gmail.com>
2015-04-23 23:01:56 -0500

    Added Unit Tests and Comments

--- test/controllers/user_controller_test.rb
+++ test/controllers/user_controller_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class UserControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "true" do
+     assert true
+   end
+
+  test "creates" do
+    user = User.create
+    assert user.save
+  end
+
+  test "should destroy user" do
+    user = User.create
+    user.destroy
+    assert_response :success
+  end
+
+  test "should index user" do
+    User.create
+    assert_response :success
+  end
+
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,15 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "creates" do
+
+    user = User.new
+    # assert_difference('User.count') do
+    # post :create, user: {uid: '1'}
+    ## end
+    assert user.save
+  end
+
+
 end
